2ffb449427b7f8eb2ccd4599956a2c1b54518be3
Jeffrey Bradley <jeffrey_bradley@dpsk12.org>
2020-11-16 10:51:51 -0700

    gh-pages

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,18 @@
 set -e
 
 # build
+echo Linting..
+npm run lint
+echo Building. this may take a minute...
 npm run build
 
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+echo Deploying..
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:jnb2387/heritage.git master:gh-pages
